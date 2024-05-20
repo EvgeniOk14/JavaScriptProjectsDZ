c5c874641487c21a179e1ec783a237f890a777a9
Evgeni.ok14 <evgeni.ok@mail.ru>
2024-05-20 14:06:57 +0300

    DZ4 Tausk 4

---
+++ 04/4.4/incrementNumber.js
@@ -0,0 +1,7 @@
+let number  = 3;
+
+function incrementNumber(number){
+    return ++number
+}
+
+console.log("Результат инкремента числа " + number + " это чило равное: " + incrementNumber(number));
